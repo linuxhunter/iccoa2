3f21b34e9b1e8e343a8ba0ebd59ec28d501b1b9a
david <linuxhunter80@gmail.com>
2023-11-17 15:58:15 +0800

    complete BLE Rke

---
+++ app/src/main/java/com/example/iccoa2/ble/Rke.kt
@@ -0,0 +1,230 @@
+package com.example.iccoa2.ble
+
+import com.payneteasy.tlv.BerTag
+import com.payneteasy.tlv.BerTlvBuilder
+import com.payneteasy.tlv.BerTlvParser
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+enum class RkeFunctionAndAction(val function: UShort, val action: UByte) {
+    DoorLock(0x0001u, 0x01u),
+    DoorUnlock(0x0001u, 0x02u),
+    CarWindowFullOpen(0x0002u, 0x01u),
+    CarWindowFullClose(0x0002u, 0x02u),
+    CarWindowPartialOpen(0x0002u, 0x03u),
+    BackTrunkOpen(0x0003u, 0x01u),
+    BackTrunkClose(0x0003u, 0x02u),
+    EngineStart(0x0004u, 0x01u),
+    EngineStop(0x0004u, 0x02u),
+    FindVehicleFlashing(0x0005u, 0x01u),
+    FindVehicleWhistling(0x0005u, 0x02u),
+    FindVehicleFlashingAndWhistling(0x0005u, 0x03u),
+    Custom(0x1001u, 0x00u)
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+class RkeRequest {
+    companion object {
+        const val FIRST_RKE_REQUEST_TAG: Int = 0x7F
+        const val SECOND_RKE_REQUEST_TAG: Int = 0x70
+        const val FUNCTION_TAG: Int = 0x80
+        const val ACTION_TAG: Int = 0x81
+    }
+    var rke: RkeFunctionAndAction = RkeFunctionAndAction.DoorLock
+
+    fun serialize(): UByteArray {
+        return BerTlvBuilder().run {
+            this.addBytes(BerTag(FIRST_RKE_REQUEST_TAG, SECOND_RKE_REQUEST_TAG), BerTlvBuilder().run {
+                this.addBytes(BerTag(FUNCTION_TAG), ByteBuffer.allocate(Short.SIZE_BYTES).run {
+                    this.order(ByteOrder.BIG_ENDIAN)
+                    this.putShort(rke.function.toShort())
+                    this.array()
+                })
+                this.addByte(BerTag(ACTION_TAG), rke.action.toByte())
+                this.buildArray()
+            })
+            this.buildArray().toUByteArray()
+        }
+    }
+    fun deserialize(buffer: UByteArray) {
+        val tlv = BerTlvParser().run {
+            this.parse(buffer.toByteArray())
+        }
+        if (tlv.find(BerTag(FIRST_RKE_REQUEST_TAG, SECOND_RKE_REQUEST_TAG)) == null ||
+            tlv.find(BerTag(FUNCTION_TAG)) == null ||
+            tlv.find(BerTag(ACTION_TAG)) == null) {
+            return
+        }
+        rke = RkeFunctionAndAction.values().first {
+            it.function == ByteBuffer.wrap(
+                tlv.find(BerTag(FUNCTION_TAG)).run {
+                    this.bytesValue
+                }, 0, 2
+            ).run {
+                this.order(ByteOrder.BIG_ENDIAN)
+                this.short.toUShort()
+            } && it.action == tlv.find(BerTag(ACTION_TAG)).run {
+                this.bytesValue[0].toUByte()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+class RkeContinuedRequest {
+    companion object {
+        const val FIRST_RKE_CONTINUED_REQUEST_TAG: Int = 0x7F
+        const val SECOND_RKE_CONTINUED_REQUEST_TAG: Int = 0x76
+        const val FUNCTION_TAG: Int = 0x80
+        const val ACTION_TAG: Int = 0x81
+        const val CUSTOM_TAG: Int = 0x88
+    }
+    var rke: RkeFunctionAndAction = RkeFunctionAndAction.DoorLock
+    var custom: UByteArray? = null
+
+    fun serialize(): UByteArray {
+        return BerTlvBuilder().run {
+            this.addBytes(BerTag(FIRST_RKE_CONTINUED_REQUEST_TAG, SECOND_RKE_CONTINUED_REQUEST_TAG), BerTlvBuilder().run {
+                this.addBytes(BerTag(FUNCTION_TAG), ByteBuffer.allocate(Short.SIZE_BYTES).run {
+                    this.order(ByteOrder.BIG_ENDIAN)
+                    this.putShort(rke.function.toShort())
+                    this.array()
+                })
+                this.addByte(BerTag(ACTION_TAG), rke.action.toByte())
+                if (custom != null) {
+                    this.addBytes(BerTag(CUSTOM_TAG), custom!!.toByteArray())
+                }
+                this.buildArray()
+            })
+            this.buildArray().toUByteArray()
+        }
+    }
+    fun deserialize(buffer: UByteArray) {
+        val tlv = BerTlvParser().run {
+            this.parse(buffer.toByteArray())
+        }
+        if (tlv.find(BerTag(FIRST_RKE_CONTINUED_REQUEST_TAG, SECOND_RKE_CONTINUED_REQUEST_TAG)) == null ||
+            tlv.find(BerTag(FUNCTION_TAG)) == null ||
+            tlv.find(BerTag(ACTION_TAG)) == null) {
+            return
+        }
+        rke = RkeFunctionAndAction.values().first {
+            it.function == ByteBuffer.wrap(
+                tlv.find(BerTag(RkeRequest.FUNCTION_TAG)).run {
+                    this.bytesValue
+                }, 0, 2
+            ).run {
+                this.order(ByteOrder.BIG_ENDIAN)
+                this.short.toUShort()
+            } && it.action == tlv.find(BerTag(RkeRequest.ACTION_TAG)).run {
+                this.bytesValue[0].toUByte()
+            }
+        }
+        custom = if (tlv.find(BerTag(CUSTOM_TAG)) != null) {
+            tlv.find(BerTag(CUSTOM_TAG)).run {
+                this.bytesValue.toUByteArray()
+            }
+        } else {
+            null
+        }
+    }
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+class RkeVerificationResponse {
+    companion object {
+        const val RANDOM_TAG: Int = 0x8A
+    }
+    var random: UByteArray = ubyteArrayOf()
+
+    fun serialize(): UByteArray {
+        return BerTlvBuilder().run {
+            this.addBytes(BerTag(RANDOM_TAG), random.toByteArray())
+            this.buildArray().toUByteArray()
+        }
+    }
+    fun deserialize(buffer: UByteArray) {
+        val tlv = BerTlvParser().run {
+            this.parse(buffer.toByteArray())
+        }
+        if (tlv.find(BerTag(RANDOM_TAG)) == null) {
+            return
+        }
+        random = tlv.find(BerTag(RANDOM_TAG)).run {
+            this.bytesValue.toUByteArray()
+        }
+    }
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+class RkeResponse {
+    companion object {
+        const val FIRST_RKE_RESPONSE_TAG: Int = 0x7F
+        const val SECOND_RKE_RESPONSE_TAG: Int = 0x72
+        const val RKE_RESPONSE_MIDDLE_TAG: Int = 0xA0
+        const val FUNCTION_TAG: Int = 0x80
+        const val ACTION_TAG: Int = 0x83
+        const val STATUS_TAG: Int = 0x89
+    }
+    var rke: RkeFunctionAndAction = RkeFunctionAndAction.DoorLock
+    var status: UShort = 0x0000u
+
+    fun serialize(): UByteArray {
+        return BerTlvBuilder().run {
+            this.addBytes(BerTag(FIRST_RKE_RESPONSE_TAG, SECOND_RKE_RESPONSE_TAG), BerTlvBuilder().run {
+                this.addBytes(BerTag(RKE_RESPONSE_MIDDLE_TAG), BerTlvBuilder().run {
+                    this.addBytes(BerTag(FUNCTION_TAG), ByteBuffer.allocate(Short.SIZE_BYTES).run {
+                        this.order(ByteOrder.BIG_ENDIAN)
+                        this.putShort(rke.function.toShort())
+                        this.array()
+                    })
+                    this.addBytes(BerTag(ACTION_TAG), ByteBuffer.allocate(Short.SIZE_BYTES).run {
+                        this.order(ByteOrder.BIG_ENDIAN)
+                        this.putShort(rke.action.toShort())
+                        this.array()
+                    })
+                    this.addBytes(BerTag(STATUS_TAG), ByteBuffer.allocate(Short.SIZE_BYTES).run {
+                        this.order(ByteOrder.BIG_ENDIAN)
+                        this.putShort(status.toShort())
+                        this.array()
+                    })
+                    this.buildArray()
+                })
+                this.buildArray()
+            })
+            this.buildArray().toUByteArray()
+        }
+    }
+    fun deserialize(buffer: UByteArray) {
+        val tlv = BerTlvParser().run {
+            this.parse(buffer.toByteArray())
+        }
+        if (tlv.find(BerTag(FIRST_RKE_RESPONSE_TAG, SECOND_RKE_RESPONSE_TAG)) == null ||
+            tlv.find(BerTag(RKE_RESPONSE_MIDDLE_TAG)) == null ||
+            tlv.find(BerTag(FUNCTION_TAG)) == null ||
+            tlv.find(BerTag(ACTION_TAG)) == null ||
+            tlv.find(BerTag(STATUS_TAG)) == null) {
+            return
+        }
+        rke = RkeFunctionAndAction.values().first {
+            it.function == ByteBuffer.wrap(
+                tlv.find(BerTag(FUNCTION_TAG)).run {
+                    this.bytesValue
+                }, 0, 2
+            ).run {
+                this.order(ByteOrder.BIG_ENDIAN)
+                this.short.toUShort()
+            } && it.action == tlv.find(BerTag(ACTION_TAG)).run {
+                this.bytesValue[1].toUByte()
+            }
+        }
+        status = ByteBuffer.wrap(
+            tlv.find(BerTag(STATUS_TAG)).run {
+                this.bytesValue
+            }, 0, 2
+        ).run {
+            this.order(ByteOrder.BIG_ENDIAN)
+            this.short.toUShort()
+        }
+    }
+}
---
+++ app/src/test/java/com/example/iccoa2/BleRkeUnitTest.kt
@@ -0,0 +1,376 @@
+package com.example.iccoa2
+
+import com.example.iccoa2.ble.RkeContinuedRequest
+import com.example.iccoa2.ble.RkeFunctionAndAction
+import com.example.iccoa2.ble.RkeRequest
+import com.example.iccoa2.ble.RkeResponse
+import com.example.iccoa2.ble.RkeVerificationResponse
+import org.junit.Test
+
+import org.junit.Assert.*
+
+@OptIn(ExperimentalUnsignedTypes::class)
+class BleRkeUnitTest {
+    @Test
+    fun create_rke_request() {
+        val request = RkeRequest().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+        }
+        assertEquals(request.rke.function.toInt(), 0x0001)
+        assertEquals(request.rke.action.toInt(), 0x01)
+    }
+    @Test
+    fun update_rke_request() {
+        val request = RkeRequest().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+        }
+        assertEquals(request.rke.function.toInt(), 0x0001)
+        assertEquals(request.rke.action.toInt(), 0x01)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.DoorUnlock
+        }
+        assertEquals(request.rke.function.toInt(), 0x0001)
+        assertEquals(request.rke.action.toInt(), 0x02)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.CarWindowFullOpen
+        }
+        assertEquals(request.rke.function.toInt(), 0x0002)
+        assertEquals(request.rke.action.toInt(), 0x01)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.CarWindowFullClose
+        }
+        assertEquals(request.rke.function.toInt(), 0x0002)
+        assertEquals(request.rke.action.toInt(), 0x02)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.CarWindowPartialOpen
+        }
+        assertEquals(request.rke.function.toInt(), 0x0002)
+        assertEquals(request.rke.action.toInt(), 0x03)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.BackTrunkOpen
+        }
+        assertEquals(request.rke.function.toInt(), 0x0003)
+        assertEquals(request.rke.action.toInt(), 0x01)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.BackTrunkClose
+        }
+        assertEquals(request.rke.function.toInt(), 0x0003)
+        assertEquals(request.rke.action.toInt(), 0x02)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.EngineStart
+        }
+        assertEquals(request.rke.function.toInt(), 0x0004)
+        assertEquals(request.rke.action.toInt(), 0x01)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.EngineStop
+        }
+        assertEquals(request.rke.function.toInt(), 0x0004)
+        assertEquals(request.rke.action.toInt(), 0x02)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.FindVehicleFlashing
+        }
+        assertEquals(request.rke.function.toInt(), 0x0005)
+        assertEquals(request.rke.action.toInt(), 0x01)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.FindVehicleWhistling
+        }
+        assertEquals(request.rke.function.toInt(), 0x0005)
+        assertEquals(request.rke.action.toInt(), 0x02)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.FindVehicleFlashingAndWhistling
+        }
+        assertEquals(request.rke.function.toInt(), 0x0005)
+        assertEquals(request.rke.action.toInt(), 0x03)
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.Custom
+        }
+        assertEquals(request.rke.function.toInt(), 0x1001)
+        assertEquals(request.rke.action.toInt(), 0x00)
+    }
+    @Test
+    fun serialize_rke_request() {
+        val request = RkeRequest().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+        }
+        val serializedRequest = request.serialize()
+        assertArrayEquals(
+            serializedRequest.toByteArray(),
+            ubyteArrayOf(
+                0x7Fu, 0x70u,
+                0x07u,
+                0x80u, 0x02u,
+                0x00u, 0x01u,
+                0x81u, 0x01u,
+                0x01u,
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun deserialize_rke_request() {
+        val buffer: UByteArray = ubyteArrayOf(
+            0x7Fu, 0x70u,
+            0x07u,
+            0x80u, 0x02u,
+            0x00u, 0x01u,
+            0x81u, 0x01u,
+            0x01u,
+        )
+        val request = RkeRequest().apply {
+            this.deserialize(buffer)
+        }
+        assertEquals(request.rke.function.toInt(), 0x0001)
+        assertEquals(request.rke.action.toInt(), 0x01)
+    }
+    @Test
+    fun create_rke_continued_request() {
+        val request = RkeContinuedRequest().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+            this.custom = ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            )
+        }
+        assertEquals(request.rke.function.toInt(), 0x0001)
+        assertEquals(request.rke.action.toInt(), 0x01)
+        assertNotNull(request.custom)
+        assertArrayEquals(
+            request.custom?.toByteArray(),
+            ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun update_rke_continued_request() {
+        val request = RkeContinuedRequest().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+            this.custom = ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            )
+        }
+        assertEquals(request.rke.function.toInt(), 0x0001)
+        assertEquals(request.rke.action.toInt(), 0x01)
+        assertNotNull(request.custom)
+        assertArrayEquals(
+            request.custom?.toByteArray(),
+            ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            ).toByteArray()
+        )
+
+        request.apply {
+            this.rke = RkeFunctionAndAction.EngineStart
+            this.custom = ubyteArrayOf(
+                0x08u, 0x09u, 0x0Au, 0x0Bu, 0x0Cu, 0x0Du, 0x0Eu, 0x0Fu
+            )
+        }
+        assertEquals(request.rke.function.toInt(), 0x0004)
+        assertEquals(request.rke.action.toInt(), 0x01)
+        assertNotNull(request.custom)
+        assertArrayEquals(
+            request.custom?.toByteArray(),
+            ubyteArrayOf(
+                0x08u, 0x09u, 0x0Au, 0x0Bu, 0x0Cu, 0x0Du, 0x0Eu, 0x0Fu
+            ).toByteArray()
+        )
+
+        request.apply {
+            this.custom = null
+        }
+        assertEquals(request.rke.function.toInt(), 0x0004)
+        assertEquals(request.rke.action.toInt(), 0x01)
+        assertNull(request.custom)
+    }
+    @Test
+    fun serialize_rke_continued_request() {
+        val request = RkeContinuedRequest().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+            this.custom = ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            )
+        }
+        val serializedRequest = request.serialize()
+        assertArrayEquals(
+            serializedRequest.toByteArray(),
+            ubyteArrayOf(
+                0x7Fu, 0x76u,
+                0x11u,
+                0x80u, 0x02u,
+                0x00u, 0x01u,
+                0x81u, 0x01u,
+                0x01u,
+                0x88u, 0x08u,
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun deserialize_rke_continued_request() {
+        val buffer: UByteArray = ubyteArrayOf(
+            0x7Fu, 0x76u,
+            0x11u,
+            0x80u, 0x02u,
+            0x00u, 0x01u,
+            0x81u, 0x01u,
+            0x01u,
+            0x88u, 0x08u,
+            0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+        )
+        val request = RkeContinuedRequest().apply {
+            this.deserialize(buffer)
+        }
+        assertEquals(request.rke.function.toInt(), 0x0001)
+        assertEquals(request.rke.action.toInt(), 0x01)
+        assertNotNull(request.custom)
+        assertArrayEquals(
+            request.custom?.toByteArray(),
+            ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun create_rke_verification_response() {
+        val response = RkeVerificationResponse().apply {
+            this.random = ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            )
+        }
+        assertArrayEquals(
+            response.random.toByteArray(),
+            ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun update_rke_verification_response() {
+        val response = RkeVerificationResponse().apply {
+            this.random = ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            )
+        }
+
+        response.apply {
+            this.random = ubyteArrayOf(
+                0x08u, 0x09u, 0x0Au, 0x0Bu, 0x0Cu, 0x0Du, 0x0Eu, 0x0Fu
+            )
+        }
+        assertArrayEquals(
+            response.random.toByteArray(),
+            ubyteArrayOf(
+                0x08u, 0x09u, 0x0Au, 0x0Bu, 0x0Cu, 0x0Du, 0x0Eu, 0x0Fu
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun serialize_rke_verification_response() {
+        val response = RkeVerificationResponse().apply {
+            this.random = ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            )
+        }
+        val serializedResponse = response.serialize()
+        assertArrayEquals(
+            serializedResponse.toByteArray(),
+            ubyteArrayOf(
+                0x8Au, 0x08u,
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun deserialize_rke_verification_response() {
+        val buffer: UByteArray = ubyteArrayOf(
+            0x8Au, 0x08u,
+            0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+        )
+        val response = RkeVerificationResponse().apply {
+            this.deserialize(buffer)
+        }
+        assertArrayEquals(
+            response.random.toByteArray(),
+            ubyteArrayOf(
+                0x00u, 0x01u, 0x2u, 0x03u, 0x04u, 0x05u, 0x06u, 0x07u
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun create_rke_response() {
+        val response = RkeResponse().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+            this.status = 0xAABBu
+        }
+        assertEquals(response.rke.function.toInt(), 0x0001)
+        assertEquals(response.rke.action.toInt(), 0x01)
+        assertEquals(response.status.toInt(), 0xAABB)
+    }
+    @Test
+    fun update_rke_response() {
+        val response = RkeResponse().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+            this.status = 0xAABBu
+        }
+
+        response.apply {
+            this.rke = RkeFunctionAndAction.DoorUnlock
+            this.status = 0xBBAAu
+        }
+        assertEquals(response.rke.function.toInt(), 0x0001)
+        assertEquals(response.rke.action.toInt(), 0x02)
+        assertEquals(response.status.toInt(), 0xBBAA)
+    }
+    @Test
+    fun serialize_rke_response() {
+        val response = RkeResponse().apply {
+            this.rke = RkeFunctionAndAction.DoorLock
+            this.status = 0xAABBu
+        }
+        val serializedResponse = response.serialize()
+        assertArrayEquals(
+            serializedResponse.toByteArray(),
+            ubyteArrayOf(
+                0x7Fu, 0x72u,
+                0x0Eu,
+                0xA0u, 0x0Cu,
+                0x80u, 0x02u,
+                0x00u, 0x01u,
+                0x83u, 0x02u,
+                0x00u, 0x01u,
+                0x89u, 0x02u,
+                0xAAu, 0xBBu,
+            ).toByteArray()
+        )
+    }
+    @Test
+    fun deserialize_rke_response() {
+        val buffer: UByteArray = ubyteArrayOf(
+            0x7Fu, 0x72u,
+            0x0Eu,
+            0xA0u, 0x0Cu,
+            0x80u, 0x02u,
+            0x00u, 0x01u,
+            0x83u, 0x02u,
+            0x00u, 0x01u,
+            0x89u, 0x02u,
+            0xAAu, 0xBBu,
+        )
+        val response = RkeResponse().apply {
+            this.deserialize(buffer)
+        }
+        assertEquals(response.rke.function.toInt(), 0x0001)
+        assertEquals(response.rke.action.toInt(), 0x01)
+        assertEquals(response.status.toInt(), 0xAABB)
+    }
+}
